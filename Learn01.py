f75bd9911ab5bb2459fd7104dd19497e4fa3a06d
Yellow_Flash <amgurikar@gmail.com>
2021-03-31 13:08:46 +0200

    learn01

--- Learn01.py
+++ Learn01.py
@@ -2,7 +2,9 @@ import pandas as pd
 from openpyxl.workbook import Workbook
 
 df_excel = pd.read_excel('regions.xlsx')
-
+df_csv = pd.read_csv('Names.csv')
+df_txt = pd.read_csv('data.txt')
+print(df.excel)
 
 
 
